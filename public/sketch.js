9d2235f3a47dd2b31bd36ac1949382ab1b29b92c
xLeDocteurx <lenoirc.fr@gmail.com>
2018-06-06 22:45:40 +0200

    the minimum is done. I added a small random color wich is synchroniszd between clients

--- public/sketch.js
+++ public/sketch.js
@@ -1,20 +1,65 @@
-var offset = 100;
+let socket;
+
+let offset = 100;
 
 function setup() {
 
     createCanvas(500, 500);
     background(255);
+
+    socket = io.connect('http://localhost:3000');
+    socket.on('mouse', newDrawing);
 }
 
 function draw() {
     stroke(0);
     if (mouseIsPressed === true) {
-        line(mouseX, mouseY, pmouseX, pmouseY);
-        // rect(mouseX, mouseY,20,10);
-        // fill(0);
-        console.log('drawing a line at : ', mouseX, mouseY);
+        // draw();
     }
 }
+
+function mouseDragged() {
+    console.log("Sendi ng : x-" + mouseX + " , y-" + mouseY);
+
+    let rr = Math.random() * 255;
+    let gg = Math.random() * 255;
+    let bb = Math.random() * 255;
+
+    var data = {
+        x: mouseX,
+        y: mouseY,
+        px: pmouseX,
+        py: pmouseY,
+        r: rr,
+        g: gg,
+        b: bb
+    };
+
+    socket.emit('mouse', data);
+    draw(rr, gg, bb);
+}
+
+function draw(r, g, b) {
+
+    // stroke(r, g, b);
+    // line(mouseX, mouseY, pmouseX, pmouseY);
+    noStroke();
+    fill(r, g, b);    
+    ellipse(mouseX, mouseY, 30, 30);
+}
+
+function newDrawing(data) {
+
+    // stroke(data.r, data.g, data.b);
+    // line(data.px, data.py, data.x, data.y);
+    noStroke();
+    fill(data.r, data.g, data.b);    
+    ellipse(data.x, data.y, 30, 30);
+}
+
+
+
+// Si la fenetre est redimentionnée :
 
 // function windowResized() {
 //     resizeCanvas(windowWidth, windowHeight);
--- server.js
+++ server.js
@@ -4,21 +4,58 @@
 // let fs = require('fs');
 // const EventEmitter = require('events');
 
-let webPort = 8080;
+let allClients = [];
 
-let socket = require('socket.io');
+let webPort = 3000;
 let express = require('express');
 
-console.log('My server is running');
+let app = express();
+
+
+// app.set('view engine', 'ejs');
+
+// app.get('/', function(req, res) {
+//   res.render('index', {
+//     posts: data.posts
+//   });
+// });
+
 
 // socket.listen(`http://localhost:${webPort}`);
-let app = express();
 app.use(express.static('public'));
 
 let server = app.listen(webPort);
+
+console.log('My server is running');
+
+let socket = require('socket.io');
+
 let io = socket(server);
 
-io.on('connnection', (socket) => {
-  console.log("socket : ");
-  console.log(socket);
+io.on('connection', (socket) => {
+  console.log("--- // Nouvelle connection");
+  console.log("Connection ID : " + socket.id);
+
+  allClients.push(socket);
+  console.log("New connected users list : ");
+  console.log(allClients);
+
+  socket.on('mouse', mouseData);
+
+  function mouseData(data) {
+
+    socket.broadcast.emit('mouse', data);
+    // io.sockets.emit('mouse', data);
+  }
+
+  socket.on('disconnect', function () {
+    console.log(socket.id + ' // Got disconnect!');
+    var i = allClients.indexOf(socket);
+
+    allClients.splice(i, 1);
+    console.log("New connected users list : ");
+    console.log(allClients);
+  });
 });
+
+
