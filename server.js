4e2e0fc8585ea038562c70e0392ff5c1d06ee1a6
xLeDocteurx <lenoirc.fr@gmail.com>
2018-06-09 19:11:02 +0200

    Tools are on the way

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ let app = express();
 // socket.listen(`http://localhost:${webPort}`);
 app.use(express.static('public'));
 
-let server = app.listen(process.env.PORT || webport);
+let server = app.listen(process.env.PORT || webPort);
 
 console.log('My server is running');
 
