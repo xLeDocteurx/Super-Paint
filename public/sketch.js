da5cf1aafbd36540ec628f53a3b9d0a485822d5d
Valentin Janvier <alencruserxx@hotmail.fr>
2018-06-10 18:57:09 +0200

    Ajout des fonctions pour modifier les troisièmes et 4emes valeurs

--- public/sketch.js
+++ public/sketch.js
@@ -5,9 +5,18 @@ let canvas_container = document.getElementById("canvas_container");
 
 let chosen_color = 125;
 let chosen_tool = "Line";
+let third_part= 50;
+let fourth_part=50;
 $(':button').click(function(){
     chosen_tool=$(this).val();
-    console.log($(this).val());
+});
+$('.three').click(function(){
+    third_part=$('#three').val();
+    console.log(third_part)
+});
+$('.four').click(function(){
+    fourth_part=$('#four').val();
+    console.log(fourth_part)
 });
 
 function setup() {
@@ -38,7 +47,9 @@ function draw() {
             px: pmouseX,
             py: pmouseY,
             tool: chosen_tool,
-            color: chosen_color
+            color: chosen_color,
+            g: third_part,
+            u: fourth_part
         };
 
         socket.emit('mouse', data);
@@ -66,14 +77,14 @@ function drawing(tool, color) {
         break;
         case "Arc":
         stroke(color);
-        arc(mouseX, mouseY, 50, 50, 0, 90);
+        arc(mouseX, mouseY, 50, 50, third_part, fourth_part);
         break;
         case "Quad":
         stroke(color);
         quad(mouseX, mouseY, mouseX+20, mouseY+35, mouseX-30, mouseY-40,mouseX+40,mouseY-59);
         break;
         case "Rekt"://Il faut rigoler dans la vie 
-        rect(mouseX-50,mouseY-28,100,56)
+        rect(mouseX-50,mouseY-28,third_part,fourth_part)
         break;
         case "Triangle":
         triangle(mouseX-15, mouseY-15, mouseX+15, mouseY-15, mouseX, mouseY+15);
@@ -104,14 +115,14 @@ function newDrawing(data) {
         break;
         case "Arc":
         stroke(data.color);
-        arc(data.x, data.y, 50, 50, 0, 90);
+        arc(data.x, data.y, 50, 50, data.g, data.u);
         break;
         case "Quad":
         stroke(data.color);
         quad(data.x, data.y, data.x+20, data.y+35, data.x-30, data.y-40,data.x+40,data.y-59);
         break;
         case "Rekt"://Il faut rigoler dans la vie 
-        rect(data.x-50,data.y-28,100,56)
+        rect(data.x-50,data.y-28,data.g,data.u)
         break;
         case "Triangle":
         triangle(data.x-15, data.y-15, data.x+15, data.y-15, data.x, data.y+15);
